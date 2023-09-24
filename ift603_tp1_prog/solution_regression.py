9edf839d01ffabeb3f5bddf15d69629621b1a609
Taoufik ASSABAR <133345891+Assabar700@users.noreply.github.com>
2023-09-23 22:09:37 -0400

    Update solution_regression.py

--- ift603_tp1_prog/solution_regression.py
+++ ift603_tp1_prog/solution_regression.py
@@ -2,15 +2,12 @@
 
 #####
 # Khaloui Oussama (23130746) .
-# AJOUTEZ VOS NOMS ET MATRICULES ICI
+# ASSABAR Taoufik (cip: Asst1001)
 # ~= À MODIFIER =~.
 ###
 
-import random
-
 import numpy as np
-from sklearn import linear_model
-
+import random
 
 class Regression:
     def __init__(self, lamb, m=1):
@@ -18,95 +15,78 @@ class Regression:
         self.w = None
         self.M = m
 
-    def fonction_base_polynomiale(M, x):
+    def fonction_base_polynomiale(self, x):
         """
         Fonction de base qui projette la donnee x vers un espace polynomial tel que mentionne au chapitre 3.
-        Si x est un scalaire, alors phi_x sera un vecteur à  M dimensions : (x^1,x^2,...,x^ M)
+        Si x est un scalaire, alors phi_x sera un vecteur à self.M dimensions : (x^1,x^2,...,x^self.M)
         Si x est un vecteur de N scalaires, alors phi_x sera un tableau 2D de taille NxM
-
-        NOTE : En mettant phi_x = x, on a une fonction de base lineaire qui fonctionne pour une regression lineaire
         """
-        # Vérifier si x est un scalaire ou un vecteur
-        """ if np.isscalar(x):
-            phi_x = np.array([x**i for i in range(1, M+1)])
+        if isinstance(x, np.ndarray):
+            return np.column_stack([x**i for i in range(1, self.M + 1)])
         else:
-            phi_x = np.vander(x, M, increasing=True)
-
-        return phi_x """
+            return np.array([x**i for i in range(1, self.M + 1)])
 
     def recherche_hyperparametre(self, X, t):
-        """
-        Trouver la meilleure valeur pour l'hyper-parametre self.M (pour un lambda fixe donné en entrée).
-
-        Option 1
-        Validation croisée de type "k-fold" avec k=10. La méthode array_split de numpy peut être utlisée 
-        pour diviser les données en "k" parties. Si le nombre de données en entrée N est plus petit que "k", 
-        k devient égal à N. Il est important de mélanger les données ("shuffle") avant de les sous-diviser
-        en "k" parties.
-
-        Option 2
-        Sous-échantillonage aléatoire avec ratio 80:20 pour Dtrain et Dvalid, avec un nombre de répétition k=10.
-
-        Note: 
-
-        Le resultat est mis dans la variable self.M
-
-        X: vecteur de donnees
-        t: vecteur de cibles
-        """
-        # AJOUTER CODE ICI
+        k = 10 if X.shape[0] >= 10 else X.shape[0]
+        maxErrorDifference = 0.1
+        minError = [1, np.max(t)]
         self.M = 1
+        tmp = -1
 
-    def entrainement(self, X, t, using_sklearn=False):
-        """
-        Entraîne la regression lineaire sur l'ensemble d'entraînement forme des
-        entrees ``X`` (un tableau 2D Numpy, ou la n-ieme rangee correspond à l'entree
-        x_n) et des cibles ``t`` (un tableau 1D Numpy ou le
-        n-ieme element correspond à la cible t_n). L'entraînement doit
-        utiliser le poids de regularisation specifie par ``self.lamb``.
+        while True:
+            indices = list(range(X.shape[0]))
+            random.shuffle(indices)
+            errorList = []
 
-        Cette methode doit assigner le champs ``self.w`` au vecteur
-        (tableau Numpy 1D) de taille D+1, tel que specifie à la section 3.1.4
-        du livre de Bishop.
-        
-        Lorsque using_sklearn=True, vous devez utiliser la classe "Ridge" de 
-        la librairie sklearn (voir http://scikit-learn.org/stable/modules/linear_model.html)
-        
-        Lorsque using_sklearn=Fasle, vous devez implementer l'equation 3.28 du
-        livre de Bishop. Il est suggere que le calcul de ``self.w`` n'utilise
-        pas d'inversion de matrice, mais utilise plutôt une procedure
-        de resolution de systeme d'equations lineaires (voir np.linalg.solve).
+            for i in range(k):
+                start = int(i * len(indices) / k)
+                end = int((i + 1) * len(indices) / k)
+                testIdx = indices[start:end]
+                trainIdx = list(set(indices) - set(testIdx))
 
-        Aussi, la variable membre self.M sert à projeter les variables X vers un espace polynomiale de degre M
-        (voir fonction self.fonction_base_polynomiale())
+                trainX = X[trainIdx]
+                trainT = t[trainIdx]
+                testX = X[testIdx]
+                testT = t[testIdx]
 
-        NOTE IMPORTANTE : lorsque self.M <= 0, il faut trouver la bonne valeur de self.M
+                self.resolution(trainX, trainT)
+                prediction = self.prediction(testX)
+                err = self.erreur(testT, prediction)
+                errorList.append(np.mean(err))
 
-        """
-        #AJOUTER CODE ICI
+            mean = np.mean(errorList)
+            print("M = {}, erreur après cross-validation : {:.4f}".format(self.M, mean))
+
+            toContinue = True if tmp == -1 else maxErrorDifference > (mean - tmp)
+
+            if toContinue:
+                if minError[1] > mean:
+                    minError[0] = self.M
+                    minError[1] = mean
+                tmp = mean
+                self.M += 1
+            else:
+                print("Dépassement de maxErrorDifference entre deux runs")
+                break
+
+        self.M = minError[0]
+        print("=> M optimal : {}, erreur de cross-validation : {:.4f}".format(minError[0], minError[1]))
+
+    def resolution(self, X, t):
+        phiX = self.fonction_base_polynomiale(X)
+        phiX = np.column_stack([np.ones(phiX.shape[0]), phiX])
+        toInvert = (np.identity(self.M + 1) * self.lamb) + (phiX.T @ phiX)
+        self.w = np.linalg.solve(toInvert, phiX.T @ t)
+
+    def entrainement(self, X, t):
         if self.M <= 0:
             self.recherche_hyperparametre(X, t)
 
-        phi_x = self.fonction_base_polynomiale(X)
-        self.w = [0, 1]
+        self.resolution(X, t)
 
     def prediction(self, x):
-        """
-        Retourne la prediction de la regression lineaire
-        pour une entree, representee par un tableau 1D Numpy ``x``.
-
-        Cette methode suppose que la methode ``entrainement()``
-        a prealablement ete appelee. Elle doit utiliser le champs ``self.w``
-        afin de calculer la prediction y(x,w) (equation 3.1 et 3.3).
-        """
-        # AJOUTER CODE ICI
-        return 0.5
+        return self.w[0] + self.fonction_base_polynomiale(x) @ self.w[1:]
 
     @staticmethod
     def erreur(t, prediction):
-        """
-        Retourne l'erreur de la difference au carre entre
-        la cible ``t`` et la prediction ``prediction``.
-        """
-        # AJOUTER CODE ICI
-        return 0.0
+        return np.power(t - prediction, 2)
